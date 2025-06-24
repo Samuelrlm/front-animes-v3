ea4c34af30f42aa348559a507b4984050d776131
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-06-24 18:41:09 -0300

    Add

---
+++ src/components/PageWrapper/index.jsx
@@ -0,0 +1,37 @@
+import instance from "@/instance/api"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Header from "../Header"
+
+export default function PageWrapper({children}){
+    const router = useRouter()
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if(!token){
+            router.push('/')
+        }
+
+        async function heartBeat(){
+            try {
+                await instance.get('/heartbeat')
+            } catch (error) {
+                localStorage.removeItem('token')
+                router.push('/')
+            }
+        }
+
+        heartBeat()
+        setLoading(false)
+    }, [])
+
+    return (
+        <div className="w-full min-h-screen flex flex-col">
+            <Header />
+            <div className="w-full h-full pt-[65px] bg-gradient-to-b from-gray-900 to-gray-800 min-h-screen">
+                {children}
+            </div>
+        </div>
+    )
+}
--- src/pages/animes/index.jsx
+++ src/pages/animes/index.jsx
@@ -1,26 +1,32 @@
+import PageWrapper from "@/components/PageWrapper"
+import instance from "@/instance/api"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
 export default function Animes(){
-    const router = useRouter()
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        const token = localStorage.getItem('token')
+    const [animes, setAnimes] = useState([])
 
-        if(!token){
-            router.push('/')
+    useEffect(() => {
+        async function getAnimes(){
+            try {
+                const response = await instance.get('/animes')
+
+                setAnimes(response.data)
+            } catch (error) {
+                console.log(error)
+            }
         }
 
-        setLoading(false)
+        getAnimes()
     }, [])
-    
+
     return(
-        <div className="">
-            {loading ? (
-                <div className="w-full h-screen flex items-center justify-center">
-                    <div className="w-12 h-12 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
-                </div>
-            ): <p>ESSA PAGINA É PRIVADA</p>}
-        </div>
+        <PageWrapper>
+            {animes.map((anime) => {
+                return (
+                    <p>{anime.name}</p>
+                )
+            })}
+        </PageWrapper>
     )
 }
