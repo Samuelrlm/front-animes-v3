45da4817f215da082d38d8b91b41d40fbed36416
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-07-15 15:43:57 -0300

    Add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,19 @@ export default function App({ Component, pageProps }) {
         position="top-right"
         autoClose={3000}
         theme="dark"
+      />
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sby906isfo");
+          `
+        }}
       />
       <Component {...pageProps} />
     </>
